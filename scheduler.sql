ab90f0acfa1a6a38ca5408b0b1edf08f085baac0
Nagyeom <130224836+nagul2@users.noreply.github.com>
2025-03-28 12:12:49 +0900

    Update scheduler.sql

--- scheduler.sql
+++ scheduler.sql
@@ -4,7 +4,7 @@ create table schedule
     id        bigint       not null primary key auto_increment,
     title     varchar(255) not null,
     writer    varchar(255) not null,
-    content   longtext
+    content   longtext,
     create_at datetime,
-    modify_at datetime,
+    modify_at datetime
 )
