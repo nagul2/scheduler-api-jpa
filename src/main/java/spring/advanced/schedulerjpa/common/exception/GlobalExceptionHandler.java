2e001145e88da81b7fbf15d4aa19a0bb10410f7b
nagul2 <hayani0000@gmail.com>
2025-03-30 14:18:53 +0900

    Feature: [NotFoundException 예외 응답 처리 로직 추가]
    1. 예외만 만들고 응답 처리를 빼먹었던 유저와 일정을 조회할 때 예외가 발생시 응답하는 처리 로직 작성

--- src/main/java/spring/advanced/schedulerjpa/common/exception/GlobalExceptionHandler.java
+++ src/main/java/spring/advanced/schedulerjpa/common/exception/GlobalExceptionHandler.java
@@ -32,12 +32,26 @@ public class GlobalExceptionHandler {
     /**
      * 중복된 Username 으로 로그인을 시도하거나 강제로 Username 검증을 건너뛰고 유저를 저장 시도할 때 예외 응답
      *
-     * @param e 중복된 유저 예외
+     * @param e 중복된 유저 예외(조상이 RuntimeException)
      * @return ErrorDto, 400 상태코드
      */
     @ExceptionHandler({DuplicatedUserException.class, DuplicatedUsernameException.class})
     public ResponseEntity<ErrorDto> duplicatedUserException(RuntimeException e) {
         log.error("[duplicatedUserException] ex: ", e);
+        ErrorDto errorDto = new ErrorDto(ErrorCode.LOGIN_FAILED.getCode(), ErrorCode.LOGIN_FAILED, e.getMessage(), LocalDateTime.now());
+
+        return new ResponseEntity<>(errorDto, ErrorCode.LOGIN_FAILED.getHttpStatus());
+    }
+
+    /**
+     * 유저나 일정을 찾지 못할 경우 발생되는 예외 응답
+     *
+     * @param e 유저, 일정 못찾는 예외(조상이 RuntimeException)
+     * @return ErrorDto, 404 상태코드
+     */
+    @ExceptionHandler({NotFoundScheduleException.class, NotFoundUserException.class})
+    public ResponseEntity<ErrorDto> notFoundException(RuntimeException e) {
+        log.error("[notFoundException] ex: ", e);
         ErrorDto errorDto = new ErrorDto(ErrorCode.LOGIN_FAILED.getCode(), ErrorCode.LOGIN_FAILED, e.getMessage(), LocalDateTime.now());
 
         return new ResponseEntity<>(errorDto, ErrorCode.LOGIN_FAILED.getHttpStatus());
