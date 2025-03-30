97e74289ba43ec3276174db6eae0219cd8c5a612
nagul2 <hayani0000@gmail.com>
2025-03-30 14:12:38 +0900

    Refactor: [빠진 날짜 포맷 적용]
    1. User 응답 DTO에 빠져있는 날짜 포맷 직렬화 적용

--- src/main/java/spring/advanced/schedulerjpa/user/domain/dto/UserFindResponseDto.java
+++ src/main/java/spring/advanced/schedulerjpa/user/domain/dto/UserFindResponseDto.java
@@ -1,5 +1,6 @@
 package spring.advanced.schedulerjpa.user.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import spring.advanced.schedulerjpa.user.domain.entity.User;
 
 import java.time.LocalDateTime;
@@ -7,6 +8,8 @@ import java.time.LocalDateTime;
 public record UserFindResponseDto(
         String username,
         String email,
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime modifyAt
 ) {
 
