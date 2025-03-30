33ec6cccfc62e15d5e92d6802101c1aa892e2abc
Nagyeom <130224836+nagul2@users.noreply.github.com>
2025-03-30 15:37:30 +0900

    Update scheduler.sql

--- scheduler.sql
+++ scheduler.sql
@@ -3,10 +3,10 @@ create table schedule
 (
     id        bigint       not null primary key auto_increment,
     title     varchar(255) not null,
-    writer    varchar(255) not null,
+    user_id   varchar(255) not null,
     content   longtext,
     create_at datetime,
-    modify_at datetime,
+    modify_at datetime
 )
 
 -- user 테이블 생성
