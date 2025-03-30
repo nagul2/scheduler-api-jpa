21b2d9184ac9191d7ddb0c138f778174ff0108e0
nagul2 <hayani0000@gmail.com>
2025-03-30 16:10:22 +0900

    Refactor: [Create SQL문 수정 및 테이블명 수정]
    1. 코드에만 반영하고 일정 테이블 생성문을 수정하지 않았던 부분을 수정

--- scheduler.sql
+++ scheduler.sql
@@ -2,11 +2,12 @@
 create table schedule
 (
     id        bigint       not null primary key auto_increment,
+    user_id   bigint,
     title     varchar(255) not null,
-    user_id   varchar(255) not null,
     content   longtext,
     create_at datetime,
-    modify_at datetime
+    modify_at datetime,
+    foreign key (user_id) references users (id)
 )
 
 -- user 테이블 생성
@@ -14,7 +15,8 @@ create table users
 (
     id        bigint       not null primary key auto_increment,
     username  varchar(255) not null unique,
-    email     varchar(255) unique,
+    password  varchar(255) unique,
+    email     varchar(255),
     create_at datetime,
     modify_at datetime
 )
